c50b9af4fe337494a7c2c57223b00bbb79ed061f
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2024-04-18 17:37:54 +0930

    better var names

--- main.py
+++ main.py
@@ -21,17 +21,20 @@ def read_urls() -> dict:
     return urls
 class Main_downloader():
     def __init__(self) -> None:
-        self.main_urls = read_urls()
+        self.contents_urls = read_urls()
         self.required_folders = ['./cache','./cache/html/','./cache/json/']
         self.create_all_required_folders()
 
-    def download_all_main_file(self):
-        for name,url in self.main_urls.items():    
+    def main(self):
+        self.download_all_contents_files()
+
+    def download_all_contents_files(self):
+        for name,url in self.contents_urls.items():    
             data = self.download_data(url)
             if self.check_type(name,data) == 'html':
-                self.download_main_html(name,data)
+                self.download_contents_html(name,data)
             else :
-                self.download_main_json(name,data)
+                self.download_contents_json(name,data)
 
     def check_type(self,name,data):
         '''TODO: check actual contents of the data and confirm if it's html or json'''
@@ -49,14 +52,14 @@ class Main_downloader():
         with open(path,'w') as f:
             json.dump(r, f, indent=4)
 
-    def download_main_json(self,name,data) -> None:
-        filename = 'main.json'
+    def download_contents_json(self,name,data) -> None:
+        filename = 'contents.json'
         path = f'./cache/json/{name}/'
         full_path = path + filename
         self.save_json(data,full_path)
 
-    def download_main_html(self,name,data) -> None:
-        filename = 'main.html'
+    def download_contents_html(self,name,data) -> None:
+        filename = 'contents.html'
         path = f'./cache/html/{name}/'
         full_path = path + filename
         self.save_html(data,full_path)
@@ -88,14 +91,14 @@ class Main_downloader():
                 os.mkdir(folder)
             if folder == './cache' : # dont create subfolder on root folder
                 continue      
-            for name in self.main_urls.keys():
+            for name in self.contents_urls.keys():
                 sub_folder =  f'{folder}/{name}/'
                 if not os.path.exists(sub_folder):
                     os.mkdir(sub_folder)
 
 class Main_parser():
     def __init__(self) -> None:
-      self.main_urls = read_urls()
+      self.contents_urls = read_urls()
       self.single_urls = {}
 
     def load_html(self,filename):
@@ -113,8 +116,8 @@ class Main_parser():
         pass
 
     def get_single_names_automedon(self):
-        html_main = './cache/html/automedon/main.html'
-        html  = self.parse_html(html_main)
+        html_contents = './cache/html/automedon/contents.html'
+        html  = self.parse_html(html_contents)
 
     def  get_get_single_urls_automedon(self):
         names = self.get_single_names_cbsol()
@@ -124,9 +127,9 @@ class Main_parser():
         pass
     
     def get_single_names_cbsol(self):
-        html_main = './cache/html/cbsol/main.html'
+        html_contents = './cache/html/cbsol/contents.html'
         # base_url = self.main_urls['cbsol']
-        html  = self.parse_html(html_main)
+        html  = self.parse_html(html_contents)
         raw_links = html.find_all('a', class_='relative')[3:]
         clean_links = [link.attrs['href'] for link in raw_links]
         return clean_links
@@ -151,7 +154,8 @@ class Main_parser():
 if __name__ == '__main__':
     clear_cache()
     downloader = Main_downloader()
-    downloader.download_all_main_file()
+    downloader.main()
+    downloader.download_all_contents_files()
     # downloader.download_gist()
     # parser = Main_parser()
     # parser.get_all_single_links()
